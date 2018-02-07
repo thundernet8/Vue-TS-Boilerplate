d925133c384e9406637761d1881ff0d822a03ffe
wuxueqian <wuxueqian@meituan.com>
2018-02-07 19:40:59 +0800

    add .vue suffix when imports

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import HelloWorld from "@/components/HelloWorld";
-import FirstPage from "@/components/FirstPage";
+import HelloWorld from "@/components/HelloWorld.vue";
+import FirstPage from "@/components/FirstPage.vue";
 
 Vue.use(Router);
 
