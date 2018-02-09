0dada225f4f055e9f407d4bdfd5510a6aa8cad7e
wuxueqian <wuxueqian@meituan.com>
2018-02-09 13:27:43 +0800

    test import less

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,20 @@
 </template>
 
 <script>
+require("./assets/styles/main.less");
+
 export default {
-  name: "App"
+    name: "App"
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
